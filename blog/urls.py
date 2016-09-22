38d2340c7b1edad780952d3deba34eda6ec29015
dimansion <dimansional@gmail.com>
2016-09-23 02:05:11 +0700

    ...

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,16 @@
 from django.conf.urls import patterns, url
-from blog import views
-from django.views.generic.base import TemplateView
+from .views import (
+	index,
+	category,
+	post_detail,
+
+	)
+
 
 urlpatterns = patterns('',
-        url(r'^$', views.index, name='index'),
-        url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),
-        url(r'^post/(?P<post_title_slug>[\w\-]+)/$', views.post_detail, name='post_detail'),
+        url(r'^$', index, name='index'),
+        url(r'^category/(?P<category_name_slug>[\w\-]+)/$', category, name='category'),
+        url(r'^post/(?P<post_title_slug>[\w\-]+)/$', post_detail, name='post_detail'),
 
 
 )
