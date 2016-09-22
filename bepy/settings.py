9e8e8e521cc20557244ac176b0de8e907c75adcf
dimansion <dimansional@gmail.com>
2016-09-23 02:28:54 +0700

    add twitter for authentication

--- bepy/settings.py
+++ bepy/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
     'allauth.socialaccount.providers.facebook',
     'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.twitter',
 ]
 
 SITE_ID = 1
