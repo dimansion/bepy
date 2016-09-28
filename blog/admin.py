408d9ee64769f8d213a83db16fd7af05bc5438b4
dimansion <dimansional@gmail.com>
2016-09-28 19:31:56 +0700

    fix admin display list

--- blog/admin.py
+++ blog/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from blog.models import Category, Post
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'category','published_date',)
-    prepopulated_fields = {'slug':('title',)}
+	list_filter = ["published_date"]
+	list_display = ('title', 'published_date',)
+	prepopulated_fields = {'slug':('title',)}
     
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
