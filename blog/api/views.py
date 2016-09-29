8afdd42246af9a61a15d533bfd173c1221e4189e
dimansion <dimansional@gmail.com>
2016-09-29 08:16:02 +0700

    add filter in restframework

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,9 +1,24 @@
+from django.db.models import Q
+
+from rest_framework.filters import (
+	SearchFilter,
+	OrderingFilter,
+	)
+
 from rest_framework.generics import (
 	CreateAPIView,
 	DestroyAPIView,
 	ListAPIView,
 	RetrieveAPIView,
 	UpdateAPIView,
+	RetrieveUpdateAPIView,
+	)
+
+from rest_framework.permissions import (
+	AllowAny,
+	IsAuthenticated,
+	IsAdminUser,
+	IsAuthenticatedOrReadOnly,
 	)
 
 from blog.models import Post
@@ -16,22 +31,40 @@ from .serializers import (
 class PostCreateAPIView(CreateAPIView):
 	queryset = Post.objects.all()
 	serializer_class = PostCreateUpdateSerializer
-
-class PostListAPIView(ListAPIView):
-	queryset = Post.objects.all()
-	serializer_class = PostListSerializer
+	permission_classes = [IsAdminUser]
 
 class PostDetailAPIView(RetrieveAPIView):
 	queryset = Post.objects.all()
 	serializer_class = PostDetailSerializer	
 	lookup_field = 'slug'
 
-class PostUpdateAPIView(UpdateAPIView):
+class PostUpdateAPIView(RetrieveUpdateAPIView):
 	queryset = Post.objects.all()
 	serializer_class = PostCreateUpdateSerializer
 	lookup_field = 'slug'
+	permission_classes = [IsAdminUser]
 
 class PostDeleteAPIView(DestroyAPIView):
 	queryset = Post.objects.all()
 	serializer_class = PostDetailSerializer
 	lookup_field = 'slug'
+	permission_classes = [IsAdminUser]
+
+class PostListAPIView(ListAPIView):
+	#queryset = Post.objects.all()
+	serializer_class = PostListSerializer
+	filter_backends = [SearchFilter, OrderingFilter]
+	search_fields = ['title', 'content', 'author__first_name']
+
+	def get_queryset(self, *args, **kwargs):
+		#queryset_list = super(PostListAPIView, self).get_queryset(*args, **kwargs)
+		queryset_list = Post.objects.all()
+		query = self.request.GET.get("q")
+		if query:
+			queryset_list = queryset_list.filter(
+					Q(title__icontains=query)|
+					Q(content__icontains=query)|
+					Q(author__first_name__icontains=query) |
+					Q(author__last_name__icontains=query)
+					).distinct()
+		return queryset_list	
