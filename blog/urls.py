03c3e02d1edfe3fd8746278f06e0778a0d6e0c0e
dimansion <dimansional@gmail.com>
2016-11-12 12:55:35 +0700

    change url on step by step tutorial

--- blog/urls.py
+++ blog/urls.py
@@ -15,8 +15,8 @@ urlpatterns =[
         url(r'^category/(?P<category_name_slug>[\w\-]+)/$', category, name='category'),
         url(r'^(?P<slug>[\w\-]+)/$', post_detail, name='post_detail'),
         url(r'^add/$', post_create),
-        url(r'^post/(?P<slug>[\w-]+)/edit/$', post_update, name='update'),
-        url(r'^post/(?P<slug>[\w-]+)/delete/$', post_delete),
+        url(r'^(?P<slug>[\w-]+)/edit/$', post_update, name='update'),
+        url(r'^(?P<slug>[\w-]+)/delete/$', post_delete),
 
 ]
 
